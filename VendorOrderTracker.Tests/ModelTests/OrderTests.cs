8aed0089a515f0ec630f8983c819ff0bf7734954
Brishna Bakshev <b.bakshev@gmail.com>
2023-03-03 11:05:34 -0800

    order test 2 - good fail because auto-implementation has not been defined

--- VendorOrderTracker.Tests/ModelTests/OrderTests.cs
+++ VendorOrderTracker.Tests/ModelTests/OrderTests.cs
@@ -15,5 +15,19 @@ namespace VendorOrderTracker.Tests
       Order newOrder = new Order ("Bread", "is done with flour", 5);
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
+
+    [TestMethod]
+    public void GetTittleDescriptionAndPrice_ReturnTittleDescriptionAndPrice()
+    {
+      string orderTitle = "Bread";
+      string orderDescription = "A food consisting of flour or meal that is moistened, kneaded into dough, and often fermented using yeast";
+      int price = 5;
+
+      Order newOrder = new Order(orderTitle, orderDescription, price);
+
+      Assert.AreEqual(orderTitle, newOrder.Title);
+      Assert.AreEqual(orderDescription, newOrder.Description);
+      Assert.AreEqual(price, newOrder.Price);
+    }
   }
 }
