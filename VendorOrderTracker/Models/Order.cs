3e6f9a14238533d157c78a61dc696efd5c8d2dbf
Brishna Bakshev <b.bakshev@gmail.com>
2023-03-03 11:30:08 -0800

    order test 5 - passed with Id being defined and returns the correct value

--- VendorOrderTracker/Models/Order.cs
+++ VendorOrderTracker/Models/Order.cs
@@ -9,12 +9,14 @@ namespace VendorOrderTracker.Models
     public string Title { get; set; }
     public string Description { get; set; }
     public int Price { get;  set; }
+    public int Id { get; }
     public Order(string orderTitle, string orderDescription, int price)
     {
       Title = orderTitle;
       Description = orderDescription;
       Price = price;
       _instances.Add(this);
+      Id = _instances.Count;
     }
 
     public static void ClearAll()
