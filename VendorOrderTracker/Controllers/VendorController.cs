29d77108651a2465d1b8e314d3bb4854245ca1fc
Brishna Bakshev <b.bakshev@gmail.com>
2023-03-03 12:08:07 -0800

    WIP: lunch

---
+++ VendorOrderTracker/Controllers/VendorController.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using VendorOrderTracker.Models;
+
+namespace VendorOrderTracker.Controllers
+{
+  public class VendorController : Controller
+  {
+    [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      List<Vendor> allVendors = Vendor.GetAll();
+      return View(allVendors);
+    }
+  }
+}
