96fb319a3bd7f45a6b8231d9a20d6ed2d4644b25
Brishna Bakshev <b.bakshev@gmail.com>
2023-03-03 08:43:45 -0800

    completed boiler plates for most of the files

---
+++ VendorOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VendorOrderTracker.Models;
+using System.Collections.Generic;
+using System;
+
+[TestClass]
+  public class VendorTests
+  {
+
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    {
+      Vendor newVendor = new Vendor ("Suzie's Cafe");
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+  }
